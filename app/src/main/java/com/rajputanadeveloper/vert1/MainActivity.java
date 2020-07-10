dc495597739d60718c6baf80ad4f4f80d2d31afe
navneethh <navneethhh1@gmail.com>
2020-07-10 19:09:27 +0530

    adding bottom navigation

--- app/src/main/java/com/rajputanadeveloper/vert1/MainActivity.java
+++ app/src/main/java/com/rajputanadeveloper/vert1/MainActivity.java
@@ -1,11 +1,20 @@
 package com.rajputanadeveloper.vert1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.rajputanadeveloper.vert1.Fragment.home_fragment;
+import com.rajputanadeveloper.vert1.Fragment.notif_fragment;
+import com.rajputanadeveloper.vert1.Fragment.profile_fragment;
+import com.rajputanadeveloper.vert1.Fragment.search_fragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +22,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
-    List<VideoItem> videoItems;
-
+    BottomNavigationView bottomNavigationView;
+    Fragment selected=null;
 
 
     @Override
@@ -22,68 +31,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ViewPager2 videoViewPager=findViewById(R.id.videoViewPager);
-       videoItems= new ArrayList<VideoItem>();
+        bottomNavigationView=findViewById(R.id.bottom_navi);
+        bottomNavigationView.setOnNavigationItemReselectedListener(nagationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.Fragment_container,new home_fragment()).commit();
 
 
 
-        VideoItem celeb=new VideoItem();
-        celeb.videoURL ="https://www.infinityandroid.com/videos/video1.mp4";
-        celeb.videoTitle ="Dewalii";
-        celeb.videoDescription ="celebrate with everyone";
-        videoItems.add(celeb);
-//
-//
-        VideoItem party=new VideoItem();
-        party.videoURL ="https://www.infinityandroid.com/videos/video2.mp4";
-        party.videoTitle ="party";
-        party.videoDescription ="your life your way";
-        videoItems.add(party);
-
-
-        VideoItem exercise=new VideoItem();
-        exercise.videoURL ="https://www.infinityandroid.com/videos/video3.mp4";
-        exercise.videoTitle ="Exercise";
-        exercise.videoDescription ="no pain no gain";
-        videoItems.add(exercise);
-
-        VideoItem git=new VideoItem();
-        git.videoURL ="https://www.infinityandroid.com/videos/video1.mp4";
-        git.videoTitle ="Github new branch";
-        git.videoDescription ="no pain no gain";
-        videoItems.add(git);
-        //this is added from cloud github
-        VideoItem gitcloud=new VideoItem();
-        gitcloud.videoURL ="https://www.infinityandroid.com/videos/video2.mp4";
-        gitcloud.videoTitle ="Github from the cloud";
-        gitcloud.videoDescription ="no pain no gain";
-        videoItems.add(gitcloud);
-
-
-        VideoItem nature=new VideoItem();
-        nature.videoURL ="https://www.infinityandroid.com/videos/video4.mp4";
-        nature.videoTitle ="Greenery";
-        nature.videoDescription ="Your every walk with nature gives more tham you seek for";
-        videoItems.add(nature);
-
-        VideoItem cill2=new VideoItem();
-        cill2.videoURL ="https://www.infinityandroid.com/videos/video5.mp4";
-        cill2.videoTitle ="Chill";
-        cill2.videoDescription ="Always chill alone daily as you will meet yourself";
-        videoItems.add(cill2);
-//
-        VideoItem cill1=new VideoItem();
-        cill1.videoURL ="https://www.infinityandroid.com/videos/video6.mp4";
-        cill1.videoTitle ="adventure";
-        cill1.videoDescription ="Always chill alone daily as you will meet yourself";
-        videoItems.add(cill1);
-
-
-
-
-
-
-
-        videoViewPager.setAdapter(new VideosAdapter(videoItems));
     }
+
+    private BottomNavigationView.OnNavigationItemReselectedListener nagationItemSelectedListener=
+            new BottomNavigationView.OnNavigationItemReselectedListener() {
+                @Override
+                public void onNavigationItemReselected(@NonNull MenuItem item) {
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selected=new home_fragment();
+                            break;
+
+                        case R.id.nav_notif:
+                            selected=new notif_fragment();
+                            break;
+                        case R.id.nav_profile:
+
+                            selected=new profile_fragment();
+                            break;
+                        case R.id.nav_search:
+
+                            selected=new search_fragment();
+                            break;
+                    }
+                    if(selected!=null){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.Fragment_container,selected).commit();
+                    }
+
+                }
+            };
+
 }
